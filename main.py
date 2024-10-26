5cb124969acee309fd204376eb6c8a18e3c1bf8d
AlexHol <alexhol.the.best@gmail.com>
2024-10-26 14:10:24 +0300

    28 leson

--- main.py
+++ main.py
@@ -1,10 +1,54 @@
 from flask import Flask, session, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(basedir, 'school.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(25), nullable=False)
+    role = db.Column(db.Integer(), default=0, nullable=False)
+
+    def __repr__(self):
+        return f'<User: {self.username}>'
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/courses')
+def courses():
+    return render_template('courses.html')
+
+
+@app.route('/pricing')
+def pricing():
+    return render_template('pricing.html')
+
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
