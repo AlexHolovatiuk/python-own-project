0fce9959e6fef5e55731c563d09bb5a64e6a9efb
AlexHol <alexhol.the.best@gmail.com>
2024-11-02 14:55:46 +0200

    29

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+from crypt import methods
+from pyexpat.errors import messages
+
 from flask import Flask, session, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash
 import os
 
+SESSION_USER_ID = 'user_id'
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(basedir, 'school.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(basedir, 'school.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config['SECRET_KEY'] = 'zHBJfUdbdzd0QaQh2sKpVYFlsljJonUccP3uexIdrzY'
 db = SQLAlchemy(app)
 
 class User(db.Model):
@@ -18,6 +23,13 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User: {self.username}>'
+
+    def check_password(self, password):
+        return check_password_hash(self.password,password)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
@@ -45,9 +57,32 @@ def pricing():
     return render_template('pricing.html')
 
 
-@app.route('/services')
-def services():
-    return render_template('services.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    message = ''
+
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        user = User.query.filter_by(email=email).first()
+
+        if not user:
+            message = 'Неправильний Email!'
+        else:
+            if user.check_password(password):
+                session[SESSION_USER_ID] = user.id
+                return redirect('/')
+
+            message = 'Неправильний пароль'
+
+    return render_template('login.html', message=message)
+
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.pop(SESSION_USER_ID, None)
+    return redirect('/')
 
 
 if __name__ == '__main__':
